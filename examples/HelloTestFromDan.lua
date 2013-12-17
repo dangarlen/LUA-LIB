bae02b85d0652516a55279b4402d993f0c026951
dangarlen <dan.garlen@rinstrum.com>
2013-12-17 17:00:42 -0500

    This is just a test to see if I can add a file to gethub via TortoiseGET.

---
+++ examples/HelloTestFromDan.lua
@@ -0,0 +1,28 @@
+-------------------------------------------------------------------------------
+-- Hello
+-- 
+-- Traditional Hello World example
+-- 
+-- Configures a rinApp application, displays 'Hello World' on screen and waits
+-- for a key press before exit
+-------------------------------------------------------------------------------
+-- Include the src directory
+package.path = package.path .. ";../src/?.lua"
+
+-- Require the rinApp module
+local rinApp = require "rinApp"
+
+
+-- Add control of an dwi at the given IP and port
+local dwi = rinApp.addK400("K401")
+
+-- Write "Hello world" to the LCD screen.
+dwi.writeBotLeft("Dan")
+dwi.writeBotRight("Was Here")
+
+-- Wait for the user to press a key on the dwi
+dwi.getKey()
+
+-- Cleanup the application and exit
+rinApp.cleanup()
+os.exit()
